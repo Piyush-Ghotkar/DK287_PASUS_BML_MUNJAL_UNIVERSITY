38c8d00c2e8986624f34ec74c01ae7dd92bfa60b
Avatar Gautam <54641256+AvatarGautam@users.noreply.github.com>
2020-08-01 14:59:21 +0530

    Add files via upload

---
+++ Android/app/src/main/java/com/example/healdon/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.healdon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void nextpage(View view){
+        Intent i=new Intent(MainActivity.this, Signup.class);
+        startActivity(i);
+    }
+
+}
